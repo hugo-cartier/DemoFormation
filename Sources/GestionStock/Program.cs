210dc35f9a480376a535bad41cb1d1161563e6f0
Benjamin Laffont <alphamax@gmail.com>
2020-11-16 16:54:23 +0100

    Patch console for building

--- Sources/GestionStock/Program.cs
+++ Sources/GestionStock/Program.cs
@@ -14,18 +14,18 @@ namespace GestionStock
     {
         static void Main(string[] args)
         {
-            BusinessStockManager MonStock = new BusinessStockManager(new DataBaseArticleManager());
-            MonStock.AddArticle("4700", "Oeufs", 1.5f, 180, true);
-            MonStock.AddArticle("4700", "Oeufs test", 5f, 1000, true);
-            MonStock.AddArticle("9000", "Lait de riz", 1.2f, 780, true);
-            MonStock.AddArticle("0096", "Pizza", 3f, 500, true);
+            BusinessStockManager MonStock = new BusinessStockManager(new DatabaseArticleManager());
+            MonStock.AddArticle("4700", "Oeufs", (decimal)1.5, 180, true);
+            MonStock.AddArticle("4700", "Oeufs test", (decimal)5, 1000, true);
+            MonStock.AddArticle("9000", "Lait de riz", (decimal)1.2, 780, true);
+            MonStock.AddArticle("0096", "Pizza", 3, 500, true);
             MonStock.AfficherArticles();
             var MaRef = MonStock.RechercheParRef("4545");
             //MonStock.SupprimerParRef("9000");
             MonStock.ModifierParRef("0096", "Pizza 3 fromages");
             var MaModif = MonStock.RechercheParRef("0096");
             Console.WriteLine(MaModif);
-            MonStock.ModifierParRef("0096", null, 4f);
+            MonStock.ModifierParRef("0096", null, 4);
             Console.WriteLine(MaModif);
             MonStock.ModifierParRef("0096", null, null, 1975);
             Console.WriteLine(MaModif);
